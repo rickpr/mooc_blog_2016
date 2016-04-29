8cf70a5c476789298abd3e997bcd130a251d3f2e
Ricardo Piro-Rael <fdisk@fdisk.co>
2016-04-29 11:22:37 -0600

    Added test for the thing

--- spec/views/posts/show.html.erb_spec.rb
+++ spec/views/posts/show.html.erb_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe "posts/show", type: :view do
   before(:each) do
     @post = assign(:post, Post.create!(
-      :title => "Title",
-      :body => "MyText"
+      title: "Title",
+      body: "MyText"
     ))
   end
 
@@ -12,5 +12,11 @@ RSpec.describe "posts/show", type: :view do
     render
     expect(rendered).to match(/Title/)
     expect(rendered).to match(/MyText/)
+  end
+
+  it "renders comments" do
+    2.times { @post.comments.create!(body: "I am a comment.") }
+    render
+    assert_select "#comments>div", text: /I am a comment/, count: 2
   end
 end
