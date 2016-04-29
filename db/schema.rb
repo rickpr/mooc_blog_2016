0c6c4d96f54435ad9964481de4e914fac2f36f88
Ricardo Piro-Rael <fdisk@fdisk.co>
2016-04-29 10:35:46 -0600

    Removed unnecessary model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160425215841) do
+ActiveRecord::Schema.define(version: 20160425215824) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "post_id"
@@ -25,13 +25,6 @@ ActiveRecord::Schema.define(version: 20160425215841) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
